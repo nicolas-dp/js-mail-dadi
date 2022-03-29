1c5f4a9fa3fb1b1aac77a2d5a507cc0c11be4c21
Nicolas <wellcomenicolas@gmail.com>
2022-03-29 15:50:02 +0200

    Add second exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+/* **************** ESERCIZIO 1 **************** */
+
 /*Mail Chiedi all’utente la sua email, 
 controlla che sia nella lista di chi può accedere, 
 stampa un messaggio appropriato sull’esito del controllo.*/
@@ -24,6 +26,38 @@ for (let i = 0; i < arrayEmail.length; i++) {
         console.log(" NON PRESENTE all'interno della lista");
     } else {
     
-        console.log("Nessuna conincidenza con gli elementi inseriti");
     }
+}
+
+/* ************************************************** */
+
+
+/* **************** ESERCIZIO 2 *****************/
+/*Gioco dei dadi. Generare un numero random da 1 a 6, 
+sia per il giocatore sia per il computer. Stabilire il vincitore, 
+in base a chi fa il punteggio più alto. Prima di partire a scrivere 
+codice poniamoci qualche domanda: 
+Che ci sia un array da qualche parte? 
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?*/
+
+//Dichiaro e inizializzo due variabili
+let randomUtente = Math.floor(Math.random() * 6) + 1;
+let randomBot = Math.floor(Math.random() * 6) + 1;
+
+//Stampo le due variabili per verificare cosa contengono
+console.log(randomUtente);
+console.log(randomBot);
+
+//Impongo una condizione
+if (randomUtente > randomBot) {
+    // Se random utente è maggiore di randomBot allora 
+    // scrive quanto riportato in basso
+    console.log("Utente hai vinto");
+} else if (randomBot > randomUtente) {
+    // Se viceversa, randomBot è maggiore di randomUtente allora 
+    // scrive quanto riportato in basso
+    console.log("Mi dispiace ha vinto il BOT");
+} else {
+    //Nei casi in cui sarà uguale allora la parita finirà in parità
+    console.log("La parita è finita il parità");
 }
